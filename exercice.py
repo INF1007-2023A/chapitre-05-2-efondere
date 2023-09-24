5064f576d21206eaa97638208bedc6b50270a4ca
efondere <97473864+efondere@users.noreply.github.com>
2023-09-24 14:31:24 -0400

    utiliser les f-string pour le formattage

--- exercice.py
+++ exercice.py
@@ -8,7 +8,6 @@ def get_bill(name, data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	result = name + "\n"
 	total_tax_free = 0
 	for item in data:
 		total_tax_free += item[INDEX_QUANTITY] * item[INDEX_PRICE]
@@ -16,9 +15,15 @@ def get_bill(name, data):
 	taxes = total_tax_free * 0.15
 	total = total_tax_free + taxes
 
-	result += f"SOUS TOTAL {total_tax_free:10.2f} $\n"
-	result += f"TAXES      {taxes:10.2f} $\n"
-	result += f"TOTAL      {total:10.2f} $"
+	entries = [
+		["SOUS TOTAL", total_tax_free],
+		["TAXES", taxes],
+		["TOTAL", total]
+	]
+
+	result = name
+	for entry in entries:
+		result += f"\n{entry[0]:<12} {entry[1]:>8.2f} $"
 
 	return result
 
@@ -28,7 +33,7 @@ def format_number(number, num_decimal_digits):
 	is_negative = number < 0
 	integer_part = int(abs(number))
 	decimal_part = abs(number) - float(integer_part)
-	decimal_part_str = str(decimal_part)
+	decimal_part_str = str(decimal_part)[2:]
 
 	integer_part_str = str(integer_part)
 	for c in range(1, len(integer_part_str) + 1):
@@ -41,7 +46,7 @@ def format_number(number, num_decimal_digits):
 		for c in range(0, num_decimal_digits):
 			if c != 0 and c % 3 == 0:
 				result += " "
-			result += decimal_part_str[c + 2]
+			result += decimal_part_str[c]
 
 	if is_negative:
 		result = "-" + result
@@ -52,10 +57,8 @@ def get_triangle(num_rows):
 	num_cols = num_rows * 2 - 1
 	result = "+" * (num_cols + 2) + "\n"
 	for row in range(1, num_rows + 1):
-		num_letters = row * 2 - 1
-		num_spaces = (num_cols - num_letters) // 2
-		result += "+" + " " * num_spaces + "A" * num_letters
-		result += " " * num_spaces + "+" + "\n"
+		triangle = 'A' * (row * 2 - 1)
+		result += f"+{triangle:^{num_cols}}+\n"
 	result += "+" * (num_cols + 2)
 	return result
 
